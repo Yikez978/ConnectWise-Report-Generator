3143191ec3850bb339a4e6b4d2881d049e7a3a0b
odonnellr <odonnellryanc@gmail.com>
2014-07-14 10:46:23 -0400

    added files to git

---
+++ web/reporting/hour_trends.py
@@ -0,0 +1,25 @@
+from datetime import date, timedelta
+
+from reporting import report
+from web.reporting import cw_request
+
+
+def datespan(startDate, endDate, delta=timedelta(days=1)):
+    currentDate = startDate
+    while currentDate < endDate:
+        yield currentDate
+        currentDate += delta
+
+for date_point in datespan(date(2014, 02, 01), date(2014, 06, 21), delta=timedelta(days=30)):
+    start_date = date_point
+    end_date = date_point + timedelta(days=30)
+    start_date = str(start_date).split("-")
+    end_date = str(end_date).split("-")
+    start_date = "/".join((start_date[1],start_date[2],start_date[0]))
+    end_date = "/".join((end_date[1],end_date[2],end_date[0]))
+    new_request = cw_request.ReportRequestData(limit=0, report="Service", company="New Jersey Urology CBO-1",
+                                        start_date=start_date, end_date=end_date)
+    new_report = report.Report(new_request.document())
+    print "\n#### Incident Hours by Group ####"
+    print "For dates: " + start_date + " through: " + end_date
+    print new_report.incident_hours_by_group()
---
+++ web/reporting/problem_management.py
@@ -0,0 +1,12 @@
+from web.reporting import report
+from web.reporting import cw_request
+
+new_request = cw_request.ReportRequestData(limit=0, start_date='01/01/2014', end_date='06/13/2014')
+new_report = report.Report(new_request.document())
+
+print "\n#### Generated URL ####"
+print new_request.url()
+
+
+print "\n#### Incident Count by Contact and Type ####"
+report.comma_separated(report.top_incidents(new_report.incident_type_count_by_contact(), 50))
